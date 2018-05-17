e67fcaece8e84ed30695c654d32dc8bc0088ef14
Max Longton <elongton@Maxs-MacBook-Pro.local>
2018-05-17 01:02:01 -0400

    completed

--- vote_processor.py
+++ vote_processor.py
@@ -6,17 +6,8 @@ def lookup(dic, key, *keys):
         return lookup(dic.get(key, {}), *keys)
     return dic.get(key)
 
-
-# datetime_object = datetime.strptime('Jun 1 2005  1:33PM', '%b %d %Y %I:%M%p')
-# datetime_object = dt.datetime.strptime('2018-03-02 01:05:03', '%Y-%m-%d %I:%M:%S')
-# datetime_object2 = dt.datetime.strptime('2018-03-02 01:06:03', '%Y-%m-%d %I:%M:%S')
-
-
-# mytime = datetime.datetime(2018, 3, 2, hour=1, minute=3, second=3, microsecond=0)
-# mytime2 = datetime.datetime(2018, 3, 2, hour=1, minute=5, second=3, microsecond=0)
-# print((datetime_object2-datetime_object).total_seconds())
-
-
+tolerance = input("How many seconds between votes is a quickvote?:")
+#this script counts votes by IP address
 csv_file = csv.DictReader(open("votes.csv"))
 ip_vote_dict = {}
 for row in csv_file:
@@ -32,18 +23,7 @@ for row in csv_file:
         else:
             ip_vote_dict[potential_title][row['User IP']] = ip_vote_dict[potential_title][row['User IP']] + 1
 
-# print(vote_dict)
-######Prints out total votes by different IP addresses#######
-# for book,ips in ip_vote_dict.items():
-#     ipTotal = 0
-#     voteTotal = 0
-#     for key,number in ips.items():
-#         ipTotal = ipTotal+1
-#         voteTotal = voteTotal + number
-#     print(str(book))
-#     print(str(ipTotal) + ' IPs' + ', ' + str(voteTotal) + ' total votes')
-#     print('----------------------------')
-
+#this script finds fraudulent behavior by comparing timestamps
 csv_file = csv.DictReader(open("votes.csv"))
 quick_vote_dict = {}
 beforestamp = None
@@ -54,9 +34,29 @@ for row in csv_file:
     if (quick_vote_dict.get(potential_title) == None):
         quick_vote_dict[potential_title] = 0
     if ((beforestamp is not None) and (beforename is not None)):
-        if ((abs((timestamp - beforestamp).total_seconds()) < tolerance) and (beforename == potential_title)):
+        if ((abs((timestamp - beforestamp).total_seconds()) < int(tolerance)) and (beforename == potential_title)):
             quick_vote_dict[potential_title] = quick_vote_dict[potential_title] + 1
     beforestamp = timestamp
     beforename = potential_title
 
-print(quick_vote_dict)
+
+final_dictionary = {}
+######Finds total votes by different IP addresses#######
+for book,ips in ip_vote_dict.items():
+    ipTotal = 0
+    voteTotal = 0
+    for key,number in ips.items():
+        ipTotal = ipTotal+1
+        voteTotal = voteTotal + number
+    final_dictionary[book] = {'ipTotal': ipTotal, 'voteTotal': voteTotal}
+
+for book,quickvotes in quick_vote_dict.items():
+    final_dictionary[book]['quickVotes'] = quickvotes
+#
+# print(final_dictionary)
+
+######Prints the results#########
+for book,votevalues in final_dictionary.items():
+    print(str(book))
+    print(str(votevalues['ipTotal']) + ' IPs, ' + str(votevalues['voteTotal']) + ' total votes, ' + str(votevalues['quickVotes']) + ' quick votes')
+    print('----------------------------')
